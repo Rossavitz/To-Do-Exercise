5b12a37c94c6bd31695603c6b35f1fedc989c7f8
Rossavitz <rcummings04@yahoo.com>
2024-03-12 18:10:36 -0400

    updated files

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const form = document.querySelector("#addTodo");
 const input = document.querySelector("#todo");
+const todoList = document.querySelector("#todoList");
+const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
-const savedTodos = JSON.parse(localStorage.getItem("todos")) || []; //get saved todos or create an empty array if there are none
-
-//loop over savedTodos, create their LI, see if they're "finished" and assign text decoration, append to todoList.
 for (let i = 0; i < savedTodos.length; i++) {
   let newTodo = document.createElement("li");
   newTodo.innerText = savedTodos[i].task;
@@ -12,38 +11,50 @@ for (let i = 0; i < savedTodos.length; i++) {
     newTodo.style.textDecoration = "line-through";
   }
   todoList.appendChild(newTodo);
-}
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const todoList = document.querySelector("#todoList");
-  const newTodo = document.createElement("li"); //create the empty 'li'
   const removeBtn = document.createElement("button"); //create the button after the 'li'
   removeBtn.innerText = "Remove Task"; //button text
-  newTodo.innerText = input.value; //giving the empty 'li' a value
   newTodo.appendChild(removeBtn); //putting the button at the end of the newly added 'li'
-  todoList.appendChild(newTodo); //attaching the new 'li' to the end of the 'ul'
-  input.value = ""; //resetting the input field to blank
+}
 
-  savedTodos.push({ task: newTodo.innerText, finished: false }); //pushing newTodo into savedTodos
-  localStorage.setItem("todos", JSON.stringify(savedTodos)); //saving to local storage
+// "adding the TODO" via events.
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //   createNewTodo(input.value); //calls function to create todo
+
+  let newTodo = document.createElement("li");
+  let todoValue = document.getElementById("todo").value;
+  newTodo.innerText = todoValue;
+  newTodo.finished = false;
+  todoList.appendChild(newTodo);
+
+  savedTodos.push({ task: newTodo.innerText, finished: false });
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+
+  const removeBtn = document.createElement("button"); //create the button after the 'li'
+  removeBtn.innerText = "Remove Task"; //button text
+  newTodo.appendChild(removeBtn); //putting the button at the end of the newly added 'li'
+
+  form.reset(); //resetting the input field to blank
 });
 
-//function to remove todo from local storage
-const removeFromLocalStorage = (elementToRemove) => {
-  updatedTodos = savedTodos.filter((t) => {
-    return t.task !== elementToRemove.innerText;
-  });
-  elementToRemove.remove();
-  localStorage.setItem("todos", JSON.stringify(updatedTodos));
-};
+function deleteTodo(index) {
+  let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+  savedTodos.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+}
 
 todoList.addEventListener("click", function (e) {
   if (e.target.tagName === "BUTTON") {
-    removeFromLocalStorage(e.target.parentElement); //run function to remove from local storage
-    e.target.parentElement.remove();
+    let li = e.target.parentElement;
+    let index = Array.prototype.indexOf.call(todoList.children, li);
+    deleteTodo(index);
+    todoList.removeChild(li);
     //if you click a button, remove the "todo"
-  } else if (e.target.tagName === "LI") {
+  }
+});
+
+todoList.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
     let clickedTodo = e.target;
     if (!clickedTodo.finished) {
       //if the clicked to do is not finished, add the line
@@ -55,7 +66,7 @@ todoList.addEventListener("click", function (e) {
     }
 
     for (let i = 0; i < savedTodos.length; i++) {
-      if (savedTodos[i].task === clickedTodo.innerText) {
+      if (savedTodos[i].task == clickedTodo.innerText) {
         savedTodos[i].finished = !savedTodos[i].finished;
         localStorage.setItem("todos", JSON.stringify(savedTodos));
       }
